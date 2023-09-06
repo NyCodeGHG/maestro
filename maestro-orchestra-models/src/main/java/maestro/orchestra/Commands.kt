84792cdbe27f6be9de0e24bef0ba0da4e0291acc
Dan Caseley <dan@caseley.me.uk>
2023-09-06 17:47:41 +0100

    Allow blank labels
    Co-authored-by: Igor Lema <48068670+igorsmotto@users.noreply.github.com>

--- maestro-orchestra-models/src/main/java/maestro/orchestra/Commands.kt
+++ maestro-orchestra-models/src/main/java/maestro/orchestra/Commands.kt
@@ -57,7 +57,7 @@ data class SwipeCommand(
 
     override fun description(): String {
         return when {
-            label != null && label != "" -> {
+            label != null -> {
                 label
             }
             elementSelector != null && direction != null -> {
@@ -303,7 +303,7 @@ data class AssertCommand(
 ) : Command {
 
     override fun description(): String {
-        if (label != null && label != ""){
+        if (label != null){
             return label
         }
         val timeoutStr = timeout?.let { " within $timeout ms" } ?: ""
@@ -386,7 +386,7 @@ data class LaunchAppCommand(
 ) : Command {
 
     override fun description(): String {
-        if (label != null && label != ""){
+        if (label != null){
             return label
         }
 
@@ -448,7 +448,7 @@ data class OpenLinkCommand(
 ) : Command {
 
     override fun description(): String {
-        return if (label != null && label != "") {
+        return if (label != null) {
             label
         } else if (browser == true) {
             if (autoVerify == true) "Open $link with auto verification in browser" else "Open $link in browser"
@@ -485,7 +485,7 @@ data class EraseTextCommand(
 ) : Command {
 
     override fun description(): String {
-        return if (label != null && label != "") {
+        return if (label != null) {
             label
         } else if (charactersToErase != null) {
             "Erase $charactersToErase characters"
@@ -679,7 +679,7 @@ data class RepeatCommand(
         val timesInt = times?.toIntOrNull() ?: 1
 
         return when {
-            label != null && label != "" -> {
+            label != null -> {
                 label
             }
             condition != null && timesInt > 1 -> {
